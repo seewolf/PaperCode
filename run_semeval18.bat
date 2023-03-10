f4e4725d26be774e0c8d3ccf44fd162cc536468f
seewolf <rainbowdriver@hotmail.com>
2023-03-10 11:56:06 +0800

    Update run_semeval18.bat

--- run_semeval18.bat
+++ run_semeval18.bat
@@ -1,7 +1,7 @@
 SET COUNT=2022
 :MyLoop
     IF "%COUNT%" == "2016" GOTO EndLoop
-    python train.py --dataset semeval18 --optimizer adamw --model_name multilabel_base_bert --pretrained_bert_name roberta-base  --bert_dim 768 --max_seq_len 128 --s 12  --seed %COUNT% --num_epoch 8 --threshold 0 --learning_rate 1e-5
+    python run.py --dataset semeval18 --optimizer adamw --model_name multilabel_base_bert --pretrained_bert_name roberta-base  --bert_dim 768 --max_seq_len 128 --s 12  --seed %COUNT% --num_epoch 8 --threshold 0 --learning_rate 1e-5
     SET /A COUNT-=1
     GOTO MyLoop
 :EndLoop
